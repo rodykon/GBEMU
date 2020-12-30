0ac9f3117af6da8d3afeb4202ca8a8b268340291
Rody Konforty <nimrodkonforty@gmail.com>
2020-12-30 21:40:53 +0200

    Laid down infrastructure for opcode implementation

--- include/cpu/cpu.h
+++ include/cpu/cpu.h
@@ -4,6 +4,7 @@
 #include <inttypes.h>
 
 
+/*    Registers    */
 struct flag_reg {
 	uint8_t nu : 4;
 	uint8_t c : 1;
@@ -22,8 +23,29 @@ struct registers {
 	uint16_t pc;
 }
 
-inline void init_registers(struct registers * regs);
+#define GET_MSB(reg) (uint8_t)(reg >> 4)
+#define GET_LSB(reg) (uint8_t)(reg & 0xff)
+#define SET_MSB(reg, val) reg = reg ^ 0xff00 + val
+#define SET_LSB(reg, val) reg = reg ^ 0x00ff + val << 4
 
+
+inline void init_registers(struct registers *regs);
+
+
+/*    Opcodes    */
+#define NUM_OPCODES 0x100
+
+typedef int(*opcode_func_t)(struct registers *regs, uint16_t address)
+
+struct opcode {
+	opcode_func_t func;
+	uint8_t size;
+	uint8_t cycles;
+}
+
+struct opcode opcodes[NUM_OPCODES];
+
+#define ADD_OPCODE(opcode, size, cycles, func) opcodes[opcode] = { func, size, cycles };
 
 
 #endif
