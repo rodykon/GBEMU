9ab90fac0276a99d5e2346d24e1529f2319ef27f
Rody Konforty <nimrodkonforty@gmail.com>
2021-04-17 18:04:03 +0300

    Added log levels

--- include/log.h
+++ include/log.h
@@ -3,6 +3,15 @@
 
 #include <stdio.h>
 
+
+// Log levels
+
+#define LDEBUG  "DEBUG: "
+#define LINFO   "INFO: "
+#define LWARN   "WARNING: "
+#define LERR    "ERROR: "
+
+// Log function definition
 #ifdef DEBUG
 #define log(fmt, ...) fprintf(stderr, "%s:%d:%s(): " fmt "\n", __FILE__, __LINE__, __func__, ##__VA_ARGS__)
 #else
